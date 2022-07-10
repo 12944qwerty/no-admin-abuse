4a9b2c20964f38ff41ab4af68aeb3aece21137f3
12944qwerty <qwerty12944qwerty@gmail.com>
2022-07-09 19:59:35 -0500

    show only on guild's you have perms

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const { Plugin } = require('powercord/entities');
-const { getModule, React } = require('powercord/webpack');
+const { getModule, React, constants } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
-const { findInReactTree, getOwnerInstance } = require('powercord/util');
+const { findInReactTree } = require('powercord/util');
 
 const Settings = require('./Settings')
 
+const Permissions = Object.assign({}, constants.Permissions);
+
 module.exports = class NoAdminAbuse extends Plugin {
     async startPlugin() {
+        const { getGuildId } = await getModule(['getLastSelectedGuildId']);
+        const { getGuild } = await getModule(['getGuild']);
+        const { can } = await getModule(['getGuildPermissions']);
+
         powercord.api.settings.registerSettings(this.entityID, {
           category: this.entityID,
           label: 'No Admin Abuse',
           render: Settings
-        });
-    
-        
+        });    
+
         const ConnectedTextChannel = await getModule((m) => m.default?.displayName === 'ConnectedTextChannel');
         inject('no-reordering-channel', ConnectedTextChannel, 'default', (args, res) => {
             if (this.settings.get('no-channel-reorder', false)) {
                 res.props.canReorderChannel = false;
             }
             return res;
-        })
+        });
 
         const id = 'toggle-channel-reordering';
         const Menu = await getModule(m => m.default?.displayName === 'Menu');
         inject('toggleable-settings', Menu, 'default', ([{children}], res) => { // Thanks Marvin/Guild Profile
             if (res.props.children.props.id !== 'guild-header-popout') return res;
+            
+            const guild = getGuild(getGuildId());
+            if (!can(Permissions.MANAGE_CHANNELS, guild)) return res;
 
             console.log(res);
 
